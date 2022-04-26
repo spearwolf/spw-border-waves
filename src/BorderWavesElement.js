e1d3de2f317d7d0d8ad9cce5397c7eb7f4911a02
Wolfger Schramm <wolfger@spearwolf.de>
2022-04-26 15:04:43 +0200

    upgrade deps

--- src/BorderWavesElement.js
+++ src/BorderWavesElement.js
@@ -1,5 +1,5 @@
 import { css, html, LitElement } from "lit";
-import { Display, getContentAreaSize } from "@spearwolf/three-display";
+import { Display, getContentAreaSize } from "@spearwolf/display3";
 import { BorderWavesApp } from "./BorderWavesApp";
 
 const CustomProperties = {
